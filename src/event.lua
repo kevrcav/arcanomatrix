761f433042b1754a99a86794fac6cec0ba5ac5dd
Kevin Cavanaugh <kevrcav@gmail.com>
2014-10-12 21:25:34 -0400

    Added some comments to the event and event manager

--- src/event.lua
+++ src/event.lua
@@ -1,4 +1,6 @@
-local event = 
+--Event: Assigned string as a type, then it can be assigned any fields needed. 
+--Thanks to Lua's lack of type checking, this just acts as a table with a lable.
+local event =
 { }
 
 function event:new(type)
--- src/eventmanager.lua
+++ src/eventmanager.lua
@@ -1,10 +1,15 @@
+-- Event manager:
+-- Stores listeners in a map of event types -> listeners for that event. 
+
 local eventmanager = {registeredListeners = {}}
 
+-- register a listener to listen for events of the given type
 function eventmanager:registerListener(eventType, listener)
   self.registeredListeners[eventType] = self.registeredListeners[eventType] or {}
   table.insert(self.registeredListeners[eventType], listener)
 end
 
+-- send the given event to all listeners of its type
 function eventmanager:sendEvent(event)
   if self.registeredListeners[event.type] then
     table.foreach(self.registeredListeners[event.type], function(i, l)
@@ -13,6 +18,7 @@ function eventmanager:sendEvent(event)
   end
 end
 
+-- Send an event and the first listener to respond will stop sending it to the rest
 function eventmanager:sendConsumableEvent(event)
   if self.registeredListeners[event.type] then
     local quitstatus = false
@@ -25,6 +31,7 @@ function eventmanager:sendConsumableEvent(event)
   end
 end
 
+-- Removes all listeners attached to the given object
 function eventmanager:removeListenersForObject(o)
   table.foreach(self.registeredListeners, function(type, listeners)
     local toRemove = {}
